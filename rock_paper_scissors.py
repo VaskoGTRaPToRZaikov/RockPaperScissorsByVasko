572df4eda8c67b99c6c58a52a54001fd01c4a225
GTRaPToR <vaskozaikov@gmail.com>
2025-09-25 18:29:48 +0300

    Updated Rock, Paper, Scissors game!

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,48 +1,73 @@
 import random
+from colorama import Fore, Style
 
 rock = "Rock"
 paper = "Paper"
 scissors = "Scissors"
+player_score = 0
+computer_score = 0
 
-player_move = input("Choose [r]ock, [p]aper or [s]scissor: ")
+while True:
+    player_move = input("Choose [r]ock, [p]aper or [s]scissor: ")
 
-if player_move == "r":
-    player_move = rock
+    if player_move == "r":
+        player_move = rock
 
-elif player_move == "p":
-    player_move = paper
+    elif player_move == "p":
+        player_move = paper
 
-elif player_move == "s":
-    player_move = scissors
+    elif player_move == "s":
+        player_move = scissors
 
-else:
-    raise SystemExit("Invalid Input. Try again...")
+    else:
+        print("Invalid Input. Try again...")
+        continue
 
-print(f"The player chose {player_move}.")
+    print(f"The player chose {player_move}.")
 
-computer_random_number = random.randint(1, 3)
+    computer_random_number = random.randint(1, 3)
 
-computer_move = ""
+    computer_move = ""
 
-if computer_random_number == 1:
-    computer_move = rock
+    if computer_random_number == 1:
+        computer_move = rock
 
-elif computer_random_number == 2:
-    computer_move = paper
+    elif computer_random_number == 2:
+        computer_move = paper
 
-else:
-    computer_move = scissors
+    else:
+        computer_move = scissors
 
-print(f"The computer chose {computer_move}.")
+    print(f"The computer chose {computer_move}.")
 
-if (player_move == rock and computer_move == scissors) \
-    or (player_move == paper and computer_move == rock) \
-    or (player_move == scissors and computer_move == paper):
-    print("You win!")
+    if (player_move == rock and computer_move == scissors) \
+        or (player_move == paper and computer_move == rock) \
+        or (player_move == scissors and computer_move == paper):
+        print(Fore.GREEN + "You win!")
+        player_score += 1
 
-elif player_move == computer_move:
-    print("Draw!")
+    elif player_move == computer_move:
+        print(Fore.YELLOW + "Draw!")
 
-else:
-    print("You lose!")
+    else:
+        print(Fore.RED + "You lose!")
+        computer_score += 1
 
+    print(Style.RESET_ALL)
+
+    print(f"Current result is player: {player_score}, computer: {computer_score}")
+
+    while True:
+        play_again = input(f"Type [y] for 'yes' to Play Again or [n] for 'no' to quit: ").lower()
+
+        if play_again == "y":
+            print("Let's pick rock, paper or scissors")
+            break
+
+        elif play_again == "n":
+            print("Thank you for playing!")
+            exit()
+
+        else:
+            print("Invalid Input! Please type [y] or [n]... ")
+            continue
